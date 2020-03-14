01b2761a5b6ca39206e329e16ecbc0725ac14255
Geraldy Leondas <geraldy.leondas@gmail.com>
2020-03-14 14:07:13 +0100

    Updates

---
+++ precache-manifest.e90f9b9172d7ddcb82afdb467d6c1281.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f4bd5fc5d54683796b6228a83eef55",
+    "url": "/geraldy1st/weather-app/index.html"
+  },
+  {
+    "revision": "79d1124ebac68e1a6b4f",
+    "url": "/geraldy1st/weather-app/static/css/main.0ccae9b7.chunk.css"
+  },
+  {
+    "revision": "7b0b4bdb6bb0c4568a6e",
+    "url": "/geraldy1st/weather-app/static/js/2.5ef651a7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/geraldy1st/weather-app/static/js/2.5ef651a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d1124ebac68e1a6b4f",
+    "url": "/geraldy1st/weather-app/static/js/main.d4443dd1.chunk.js"
+  },
+  {
+    "revision": "8aafea9b31d8bf81d318",
+    "url": "/geraldy1st/weather-app/static/js/runtime-main.7a22dfe7.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/geraldy1st/weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/geraldy1st/weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
